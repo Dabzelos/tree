5267421c6c441d9fbc712b9b65b36f457e18302e
Rudnev Danila <dabzelos@gmail.com>
2024-04-28 20:00:18 +0300

    moved common functions to tree abs class

--- src/trees/interfaces/tree.kt
+++ src/trees/interfaces/tree.kt
@@ -1,5 +1,6 @@
 package trees.interfaces
 
+import trees.nodes.BSNode
 
 
 /**
@@ -18,17 +19,33 @@ abstract class Tree<K : Comparable<K>, V, N : Node<K, V, N>> {
     */
     var root: N? = null
 
-    //
-    //
-    //
     abstract fun insert(key: K, value: V)
     abstract fun delete(key: K)
+    abstract fun createNode(key: K, value: V): N
+
 
     fun search(key: K): V? {
         return verticeSearch(key, root)?.value
     }
 
-    private fun verticeSearch(key: K, treeroot: N?): N? {
+    protected fun insertVertice(key: K, value: V, currentNode: N?): N? {
+        if (currentNode == null) {
+            return createNode(key, value)
+        }
+        if (currentNode.key == key) {
+            currentNode.value = value
+        }
+        if (currentNode.key < key) {
+            currentNode.right = insertVertice(key, value, currentNode.right)
+        }
+
+        if (currentNode.key > key) {
+            currentNode.left = insertVertice(key, value, currentNode.left)
+        }
+        return currentNode
+    }
+
+    protected fun verticeSearch(key: K, treeroot: N?): N? {
         if (treeroot == null) {
             return null
         } else if (key < treeroot.key) {
@@ -39,4 +56,42 @@ abstract class Tree<K : Comparable<K>, V, N : Node<K, V, N>> {
             return treeroot
         }
     }
+
+    protected fun findMinKey(node: N?): N {
+        return if (node?.left == null) {
+            node!!
+        } else {
+            findMinKey(node.left)
+        }
+    }
+
+    protected fun deleteVertice(key: K, currentNode: N?): N? {
+        if (currentNode == null) {
+            return null
+        } else if (currentNode.key == key) {
+            if (currentNode.right == null && currentNode.left == null) {
+                return null
+            } else if (currentNode.right == null) {
+                return currentNode.left
+            } else if (currentNode.left == null) {
+                return currentNode.right
+            } else {
+                var nodeMinKey: N = findMinKey(currentNode.right)
+                var minkey = nodeMinKey.key
+                var minvalue = nodeMinKey.value
+                currentNode.key = minkey
+                currentNode.value = minvalue
+                currentNode.right = deleteVertice(minkey, currentNode.right)
+            }
+
+
+        } else if (currentNode.key < key) {
+            currentNode.right = deleteVertice(key, currentNode.left)
+            return currentNode
+        }
+
+        currentNode.left = deleteVertice(key, currentNode.left)
+        return currentNode
+    }
+
 }
--- src/trees/tree/BStree.kt
+++ src/trees/tree/BStree.kt
@@ -14,58 +14,9 @@ class BStree<K : Comparable<K>, V> : Tree<K, V, BSNode<K, V>>() {
         root = deleteVertice(key, root)
     }
 
-    private fun insertVertice(key: K, value: V, currentNode: BSNode<K, V>?): BSNode<K, V> {
-        if (currentNode == null) {
-            return BSNode(key, value)
-        }
-        if (currentNode.key == key) {
-            currentNode.value = value
-        }
-        if (currentNode.key < key) {
-            currentNode.right = insertVertice(key, value, currentNode.right)
-        }
 
-        if (currentNode.key > key) {
-            currentNode.left = insertVertice(key, value, currentNode.left)
-        }
-        return currentNode
-    }
-
-    private fun deleteVertice(key: K, currentNode: BSNode<K, V>?): BSNode<K, V>? {
-        if (currentNode == null) {
-            return null
-        } else if (currentNode.key == key) {
-            if (currentNode.right == null && currentNode.left == null) {
-                return null
-            } else if (currentNode.right == null) {
-                return currentNode.left
-            } else if (currentNode.left == null) {
-                return currentNode.right
-            } else {
-                var nodeMinKey: BSNode<K, V> = findMinKey(currentNode.right)
-                var minkey = nodeMinKey.key
-                var minvalue = nodeMinKey.value
-                currentNode.key = minkey
-                currentNode.value = minvalue
-                currentNode.right = deleteVertice(minkey, currentNode.right)
-            }
-
-
-        } else if (currentNode.key < key) {
-            currentNode.right = deleteVertice(key, currentNode.left)
-            return currentNode
-        }
-
-        currentNode.left = deleteVertice(key, currentNode.left)
-        return currentNode
-    }
-
-    private fun findMinKey(node: BSNode<K, V>?): BSNode<K, V> {
-        return if (node?.left == null) {
-            node!!
-        } else {
-            findMinKey(node.left)
-        }
+    override fun createNode(key: K, value: V): BSNode<K, V> {
+        return BSNode(key, value)
     }
 
 }
